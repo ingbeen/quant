5caa4c614cdda96f96e9fbf87db74de0f13d492d
ingbeen <ingbeen@naver.com>
2025-12-26 22:09:22 +0900

    리팩토링 / utils 공개 API 정리 (내부 구현 함수 제거)

--- src/qbt/utils/__init__.py
+++ src/qbt/utils/__init__.py
@@ -1,18 +1,15 @@
 """QBT Utils Package"""
 
-from .formatting import Align, format_cell, format_row
-from .logger import get_logger, set_log_level, setup_logger
+from .formatting import Align
+from .logger import get_logger, setup_logger
 from .parallel_executor import execute_parallel, execute_parallel_with_kwargs
 
 __all__ = [
     # Logger
     "setup_logger",
     "get_logger",
-    "set_log_level",
     # Formatting
     "Align",
-    "format_cell",
-    "format_row",
     # Parallel Execution
     "execute_parallel",
     "execute_parallel_with_kwargs",
